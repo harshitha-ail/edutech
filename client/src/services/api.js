413db939d3dbed9d88de977201fa54c0659980bf
Harshitha <harshithaail54@gmail.com>
2026-09-24 16:35:52 +0530

    added base

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 //const BASE_URL = "http://localhost:5000/api";
 //const BASE_URL = import.meta.env.development.local.VITE_API_BASE_URL || "/api";
-const BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5000"}/api`;
+const BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:5000/api"}/api`;
 
 async function request(endpoint, options = {}) {
   const url = `${BASE_URL}${endpoint}`;
